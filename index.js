f60d3f19bad5e5d55835eb4e1877cd3edb1866f4
Rafael Hengles <rhengles@gmail.com>
2015-05-12 14:46:29 -0300

    export all templates in one module for easier loading

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
 function precompileAMD(templates, opts) {
-	var out = '', name, template, eol;
+	var out = '', name, template, eol, moduleName;
 	opts = opts || {};
 	eol = opts.eol || '\n';
+	moduleName = opts.moduleName
+		? JSON.stringify(opts.moduleName)+', '
+		: '';
+
+	out = 'define('+moduleName+'function() {' + eol +
+		'var templates = {};' + eol;
 
 	for ( var i = 0; i < templates.length; i++ ) {
 		name = JSON.stringify(templates[i].name);
 		template = templates[i].template;
 
-		out += 'define(' + name + ', function() {' + eol +
-			template + eol + '});' + eol;
+		out += 'templates[' + name + '] = ' +
+			'(function() {' + eol + template + eol +
+			'})();' + eol;
 	}
+	out += 'return templates;' + eol + '});' + eol;
 	return out;
 }
 
